97fe7f2af4d93dba8c5667177b18db886936bdad
Myles Allen <mylesvallen@icloud.com>
2020-03-25 21:58:57 -0700

    folder renamed, same files

--- README.md
+++
@@ -1,2 +0,0 @@
-# myles-allen-portfolio
-Hello Tech World! My name is Myles Allen and this here is my personal website. 
---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from mylesallen.com'"; //letter subject
+	$to = 'mylesvallen@icloud.com'; // Enter Your E-mail
+	$from = 'owner@myles.allen.com'; // Admin e-mail
+	$fromName = 'Myles V Allen'; // Your company name
+	$charset = 'UTF-8';
+?>
